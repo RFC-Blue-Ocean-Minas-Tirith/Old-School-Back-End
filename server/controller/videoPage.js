8cad0c0abd06affd782c6bf3f4156eed07045718
Kevin Moyer <kevin7.moyer@gmail.com>
2022-10-03 16:08:16 -0500

    merged from main

--- server/controller/videoPage.js
+++ server/controller/videoPage.js
@@ -3,6 +3,12 @@ var models = require('../model/videoPage.js');
 module.exports = {
   vote: function(req, res)  {
     console.log(req.body);
-    return models.vote(req.body);
+    return models.vote(req.body)
+      .then((res) => {
+        res.status(201).send();
+      })
+      .catch((err) => {
+        res.status(500).send(err)
+      })
   },
 }
