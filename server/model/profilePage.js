6ddbaa62a54eea5cdbfe800bc41e787a17561a0a
Kevin Moyer <kevin7.moyer@gmail.com>
2022-10-06 10:03:13 -0500

    removed console.log

--- server/model/profilePage.js
+++ server/model/profilePage.js
@@ -2,7 +2,6 @@ var db = require('../db/db.js');
 
 module.exports = {
   updateFavorite: ( currentUser, user ) => {
-    console.log(currentUser, user);
     return db.UserData.findOneAndUpdate({ username: currentUser.username }, { $push: { favCreator: user } })
   },
   unFavorite: (currentUser, user ) => {
